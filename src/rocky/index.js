952821194aca122b6edc81cb012a155bbaaa12ef
Edward Dam <edward.dam@gmail.com>
2017-01-05 11:27:24 +0000

    Updated to v1.5
    Support Added for Chalk (Pebble Time Round).

--- src/rocky/index.js
+++ src/rocky/index.js
@@ -6,10 +6,10 @@ var rocky  = require('rocky');
 
 // Draw Line
 
-function drawLine(ctx, linewidth, color, position) {
+function drawLine(ctx, linewidth, color, position, width) {
   ctx.lineWidth   = linewidth;
   ctx.strokeStyle = color;
-  ctx.strokeRect(0, position, 144, 0);
+  ctx.strokeRect(0, position, width, 0);
 }
 
 // Draw Box
@@ -55,52 +55,56 @@ rocky.on('minutechange', function(event) {
 rocky.on('draw', function(event) {
   var ctx = event.context;
   
+  // Define Screen Size
+  var mx = ctx.canvas.unobstructedWidth;
+  var my = ctx.canvas.unobstructedHeight;
+  
   // Clear Canvas
-  ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+  ctx.clearRect(0, 0, mx, my);
+  
+  // Define Centre of Screen
+  var cx = mx / 2;
+  var cy = my / 2;
   
   // Draw Lines
-  drawLine(ctx, 2, 'white', 39);
-  drawLine(ctx, 2, 'white', 141);
+  drawLine(ctx, 2, 'white', cy - 45, mx);
+  drawLine(ctx, 2, 'white', cy + 57, mx);
   
   // Draw Box
-  drawBox(ctx, 'gray', 67,   9,  69, 19);
-  drawBox(ctx, 'white', 0,  65, 144, 50);
-  drawBox(ctx, 'gray', 60, 148,  24, 17);
-  drawBox(ctx, 'gray', 55, 148,  2,  17);
-  drawBox(ctx, 'gray', 87, 148,  2,  17);
-  
-  // Define Screen Size
-  var width  = ctx.canvas.unobstructedWidth;
-  var height = ctx.canvas.unobstructedHeight;
-  
+  drawBox(ctx, 'white', 0,       cy - 19, mx, 50);
+  drawBox(ctx, 'gray',  cx - 5,  cy - 75, 69, 19);
+  drawBox(ctx, 'gray',  cx - 12, cy + 64, 24, 17);
+  drawBox(ctx, 'gray',  cx - 17, cy + 64, 2,  17);
+  drawBox(ctx, 'gray',  cx + 15, cy + 64, 2,  17);
+
   // Draw Fixed Text
-  drawText(ctx, 'pebble',       'white', 'left',   '24px bold Gothic', 10,         0);
-  drawText(ctx, 'digital v1.4', 'black', 'right',  '14px bold Gothic', width - 10, 9);
-  drawText(ctx, 'water',        'gray',  'left',   '14px bold Gothic', 13,         height - 20);
-  drawText(ctx, 'WR',           'black', 'center', '18px bold Gothic', width / 2,  height - 24);
-  drawText(ctx, 'resist',       'gray',  'right',  '14px bold Gothic', width - 12, height - 20);
+  drawText(ctx, 'pebble',       'white', 'right',  '24px bold Gothic', cx - 13, cy - 84);
+  drawText(ctx, 'digital v1.5', 'black', 'left',   '14px bold Gothic', cx - 4,  cy - 75);
+  drawText(ctx, 'water',        'gray',  'right',  '14px bold Gothic', cx - 23, cy + 64);
+  drawText(ctx, 'WR',           'black', 'center', '18px bold Gothic', cx,      cy + 60);
+  drawText(ctx, 'resist',       'gray',  'left',   '14px bold Gothic', cx + 23, cy + 64);
   
   // Draw Time
   var dateHour   = new Date().toLocaleTimeString(undefined, {hour:   '2-digit'});
   var dateMinute = new Date().toLocaleTimeString(undefined, {minute: '2-digit'});
   var time       = dateHour + ":" + dateMinute;
-  drawText(ctx, time, 'black', 'center', '49px Roboto-subset', width / 2, height / 2 - 26);
+  drawText(ctx, time, 'black', 'center', '49px Roboto-subset', cx, cy - 26);
 
   // Draw Date
   var dateDay   = new Date().toLocaleDateString(undefined, {day:   'long'});
   var dateDate  = new Date().toLocaleDateString(undefined, {day:   'numeric'});
   var dateMonth = new Date().toLocaleDateString(undefined, {month: 'short'});
   var date      = dateDay + " " + dateDate + " " + dateMonth;
-  drawText(ctx, date, 'white', 'center', '18px bold Gothic', width / 2,  height / 2 - 44);
+  drawText(ctx, date, 'white', 'center', '18px bold Gothic', cx,  cy - 44);
   
   // Draw Template
-  drawText(ctx, 'Location ?°C', 'white', 'center', '18px bold Gothic', width / 2, height - 53);
+  drawText(ctx, 'Location ?°C', 'white', 'center', '18px bold Gothic', cx, cy + 31);
   
   // Draw Weather
   if (api) {
-    ctx.clearRect(0, 121, 144, 12);
+    ctx.clearRect(0, cy + 37, mx, 12);
     var city        = api.location;
     var temperature = api.temperature;
-    drawText(ctx, city + ' ' + temperature + '°C', 'white', 'center', '18px bold Gothic', width / 2, height - 53);
+    drawText(ctx, city + ' ' + temperature + '°C', 'white', 'center', '18px bold Gothic', cx, cy + 31);
   }
 });
